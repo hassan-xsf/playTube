7f34d9f95fe3a495741b555f8b7332c4004a8f03
Hassan <hassanshakil4569@gmail.com>
2024-09-02 21:04:26 +0500

    Update proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'https://play-tube-backend.vercel.app',
     },
   },
   plugins: [react()],
